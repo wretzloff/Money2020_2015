c178ff344a1eaee97c013c3cb86f70a42f94bd98
Will Retzloff <wretzloff@gmail.com>
2015-10-24 16:12:12 -0700

    sample payment logic

--- web2py/applications/Money2020Project/controllers/api.py
+++ web2py/applications/Money2020Project/controllers/api.py
@@ -3,14 +3,32 @@ def performTransaction():
     def GET():
         return 'Hello World from performTransaction GET'
     def POST(*args,**vars):
-        return 'Hello World from performTransaction POST'
+        import simplify
+		simplify.public_key = "YOUR_PUBLIC_API_KEY"
+		simplify.private_key = "YOUR_PRIVATE_API_KEY"
+		payment = simplify.Payment.create({
+       "card" : {
+            "number": "5555555555554444",
+            "expMonth": 11,
+            "expYear": 15,
+            "cvc": "123"
+        },
+        "amount" : "1000",
+        "description" : "prod description",
+        "currency" : "USD"
+})
+if payment.paymentStatus == 'APPROVED':
+    print "Payment approved"
+		
+		return 'Hello World from performTransaction POST'
     def PUT(*args,**vars):
         return ''
     def DELETE():
         return ''
     return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
 
-# example 
+
+
 @request.restful()
 def testEndpoint():
     def GET():
@@ -22,3 +40,4 @@ def testEndpoint():
     def DELETE():
         return ''
     return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
+
