0368e0aa1e8efe63dd1059abe903ed620002afea
Will Retzloff <wretzloff@gmail.com>
2015-10-24 21:58:56 -0700

    Fleshing out getItemInfo

--- web2py/applications/Money2020Project/controllers/api.py
+++ web2py/applications/Money2020Project/controllers/api.py
@@ -71,7 +71,14 @@ def testEndpoint():
 @request.restful()
 def getItemInfo():
     def GET(itemID):
-        return itemID
+        print itemID
+        itemName = None
+        itemPrice = None
+        if itemID == '038000635502':
+            itemName = 'Kellogs Special K Original Toasted Rice Cereal'
+            itemPrice = '1.23'
+        returnString = "{\"itemID\":" + itemID + ",\"itemName\":" + itemName + ",\"itemPrice\":" + itemPrice + "}"
+        return returnString
     def POST(*args,**vars):
         return 'Hello World from testEndpoint POST'
     def PUT(*args,**vars):
