113f5203a8e9d8c8fa7bff453e14e83c9febad32
anshaik <anshaik@hotmail.com>
2015-10-24 20:59:00 -0500

    json stuff

--- web2py/applications/Money2020Project/controllers/api.py
+++ web2py/applications/Money2020Project/controllers/api.py
@@ -1,9 +1,10 @@
 @request.restful()
-def performTransaction():
+def testEndpoint():
     def GET():
-    	#import simplify
-        #simplify.public_key = "sbpb_MzZiNTQ2MTAtMGY2ZS00NGI4LWEzNzEtYjQyMTU4NzhiM2Vj"
-        #simplify.private_key = "WuHzM3OXNN5eO1kWM8dRJtYNzgfbF6dtA+w8FElar/N5YFFQL0ODSXAOkNtXTToq"
+    	import simplify
+    	import json
+        simplify.public_key = "sbpb_MzZiNTQ2MTAtMGY2ZS00NGI4LWEzNzEtYjQyMTU4NzhiM2Vj"
+        simplify.private_key = "WuHzM3OXNN5eO1kWM8dRJtYNzgfbF6dtA+w8FElar/N5YFFQL0ODSXAOkNtXTToq"
 
 		#Required parameter: 'merchantID'
         #Contains an ID that identifies who is being paid (Costco, Walmart, etc.)
@@ -11,8 +12,10 @@ def performTransaction():
 
 		#Required parameter: 'items'
 		#Contains a JSON string representing an array of items that were purchased.
-        itemsString = "{\"items\": [{\"name\": \"tomato\",\"price\": \"2.93\"},{\"name\": \"tooth brush\",\"price\": \"9.99\"}]}"
+        itemsString = json.loads("{\"items\": [{\"name\": \"tomato\",\"price\": \"2.93\"},{\"name\": \"tooth brush\",\"price\": \"9.99\"}]}")
         print itemsString #debugging 
+        print "this is some thing blah \n"
+        print itemsString[0]
         #request.post_vars['items']
         #itemsArray = httpFunctions.convertJsonToArray(itemsJson)
 
@@ -59,17 +62,6 @@ def performTransaction():
 		#Save receipt to database
 		#TODO
         return 'Hello World from performTransaction GET'
-    def POST(*args,**vars):
-        return 'Hello World from performTransaction POST'
-	def PUT(*args,**vars):
-		return ''
-	def DELETE():
-		return ''
-	return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
-
-@request.restful()
-def testEndpoint():
-    def GET():
         return 'Hello World from testEndpoint GET'
     def POST(*args,**vars):
         return 'Hello World from testEndpoint POST'
