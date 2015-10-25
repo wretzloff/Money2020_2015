45d2ccb069fc6381ef457ae452ce376271333e6a
Will Retzloff <wretzloff@gmail.com>
2015-10-25 01:00:57 -0700

    Building database

--- web2py/applications/Money2020Project/models/db.py
+++ web2py/applications/Money2020Project/models/db.py
@@ -81,6 +81,8 @@ use_janrain(auth, filename='private/janrain.key')
 ## >>> db.mytable.insert(myfield='value')
 ## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
 ## >>> for row in rows: print row.id, row.myfield
+db.define_table('Merchants',Field('name','string'),migrate=False)
+db.define_table('Customer',Field('name','string'),Field('age','string'),migrate=False)
 #########################################################################
 
 ## after defining tables, uncomment below to enable auditing
